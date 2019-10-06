14d158bfd4b3cba404bea7271ef3e91ab9120b76
Konrad Wojda <kwojda@sointeractive.pl>
2019-10-06 18:45:28 +0200

    added modal with order summary

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 class App extends Component {
     render() {
         return (
-            <Layout>
-                <BurgerBuilder />
-            </Layout>
+            <div>
+                <Layout>
+                    <BurgerBuilder />
+                </Layout>
+            </div>
         );
     };
 }
