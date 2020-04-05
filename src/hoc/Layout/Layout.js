57457415e91c02f9944be868e5e99c0bbcda7f05
Konrad Wojda <kwojda@sointeractive.pl>
2020-04-05 19:59:17 +0200

    changed the files structure

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 
-import Auxiliary from '../../hoc/Auxiliary';
+import Auxiliary from '../Auxiliary/Auxiliary';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
@@ -19,7 +19,7 @@ class Layout extends Component {
             return { showSideDrawer: !this.state.showSideDrawer };
         });
     }
-
+ 
     render() {
         return (
             <Auxiliary>
